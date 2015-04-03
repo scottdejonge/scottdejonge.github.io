2f774038ca94a80aa03d88581dae86e68b26ca32
Scott de Jonge <dejonge.scott@gmail.com>
2015-04-03 13:30:58 +1000

    Title

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<title>HTML 5</title>
+		<title>Scott de Jonge</title>
 		<meta name="description" content="">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="/generated/styles/styles.css">
