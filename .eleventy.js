7487907d3fe75dc9a8a07e48c32e77a40cf9ab06
Scott de Jonge <dejonge.scott@gmail.com>
2021-01-04 15:04:03 +1100

    Remove package

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,10 @@
-
 module.exports = function(config) {
-  config.setUseGitIgnore(false);
   config.addNunjucksFilter('timestamp', path => `${path}?v=${Date.now()}`);
-  config.addWatchTarget('./src/assets/css');
   config.addPassthroughCopy({ 'src/assets/favicon': 'public/favicon' });
   config.addPassthroughCopy({ 'src/assets/images': 'public/images' });
   config.addPassthroughCopy({ 'src/public': 'public' });
+  config.addWatchTarget('./src/assets/css');
+  config.setUseGitIgnore(false);
 
   return {
     dir: {
