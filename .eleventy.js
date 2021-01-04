1568b08b9dae057537f08ab688d712b949e8415d
Scott de Jonge <dejonge.scott@gmail.com>
2021-01-04 15:28:52 +1100

    Markdown footnotes

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,21 @@
+const markdownIt = require('markdown-it');
+const markdownItFootnote = require('markdown-it-footnote');
+
+let markdownLibrary = markdownIt({
+  html: true,
+  breaks: true,
+  linkify: true,
+  typographer: true,
+})
+.use(markdownItFootnote);
+
 module.exports = function(config) {
   config.addNunjucksFilter('timestamp', path => `${path}?v=${Date.now()}`);
   config.addPassthroughCopy({ 'src/assets/favicon': 'public/favicon' });
   config.addPassthroughCopy({ 'src/assets/images': 'public/images' });
   config.addPassthroughCopy({ 'src/public': 'public' });
   config.addWatchTarget('./src/assets/css');
+  config.setLibrary('md', markdownLibrary);
   config.setUseGitIgnore(false);
 
   return {
